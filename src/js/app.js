9b1e6aeb963260ca4a1c2b9463fc050f270dc3b1
Junior Trifonov <78592455+Tesnord@users.noreply.github.com>
2023-01-27 18:23:27 +0300

    Git init

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+if (document.querySelector('.main')) {
+
+    const button = document.querySelector('.tn-btn__primary')
+    const handleClick = () => {
+        if (true) {
+            window.location.href = 'question.html'
+            // window.location.href = 'answer.html'
+        } else {
+            document.querySelector('.js_btn').click()
+        }
+    };
+    
+    button.addEventListener("click", handleClick);
+
+}
