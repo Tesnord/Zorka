7421fa0bc12e2766679932b0e105439396d9e9c1
Fenickss <denis.suxov.1991@mail.ru>
2023-01-28 17:41:24 +0300

    api

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 if (document.querySelector(".main")) {
   const button = document.querySelector(".tn-btn__primary");
   const handleClick = () => {
-    if (false) {
+    if (true) {
       window.location.href = "question.html";
       // window.location.href = 'answer.html'
     } else {
@@ -11,3 +11,12 @@ if (document.querySelector(".main")) {
 
   button.addEventListener("click", handleClick);
 }
+
+// API=======================================================
+// ===============================================================
+
+const REQUEST_URL = "https://jsonplaceholder.typicode.com/users";
+
+fetch(REQUEST_URL)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
