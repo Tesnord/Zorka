38c1b48eb495c51df1166dc7fde1a60df5b2d0a1
Fenickss <denis.suxov.1991@mail.ru>
2023-01-29 17:15:57 +0300

    radio

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,17 @@
+const REQUEST_URL = "https://jsonplaceholder.typicode.com/users";
+
+function sendRequest() {
+  return fetch(REQUEST_URL);
+}
+sendRequest("GET", REQUEST_URL)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
 if (document.querySelector(".main")) {
   const button = document.querySelector(".tn-btn__primary");
   const handleClick = () => {
     if (true) {
       window.location.href = "question.html";
-      // window.location.href = 'answer.html'
     } else {
       document.querySelector(".js_btn").click();
     }
@@ -14,12 +22,3 @@ if (document.querySelector(".main")) {
 
 // API=======================================================
 // ===============================================================
-
-const REQUEST_URL = "https://jsonplaceholder.typicode.com/users";
-
-function sendRequest() {
-  return fetch(REQUEST_URL);
-}
-sendRequest("GET", REQUEST_URL)
-  .then((response) => response.json())
-  .then((json) => console.log(json));
