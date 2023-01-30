83d0802f9ece479585e82d74ba52c977a7347fe9
Junior Trifonov <78592455+Tesnord@users.noreply.github.com>
2023-01-30 16:05:20 +0300

    FIX

--- src/js/app.js
+++ src/js/app.js
@@ -1,73 +1,72 @@
-if (document.querySelector(".main")) {
-  const button = document.querySelector(".tn-btn__primary");
-  const handleClick = () => {
-    if (true) {
-      window.location.href = "question.html";
-    } else {
-      document.querySelector(".js_btn").click();
-    }
-  };
-
-  button.addEventListener("click", handleClick);
-}
-
 // API=======================================================
 // ===============================================================
-const REQUEST_URL = "https://jsonplaceholder.typicode.com/users";
+if (document.querySelector(".main")) {
+  
+  const REQUEST_URL = "https://jsonplaceholder.typicode.com/users/12";
 
-function sendRequest() {
-  return fetch(REQUEST_URL);
+  function sendRequest() {
+    return fetch(REQUEST_URL);
+  }
+
+  document.querySelector(".tn-btn__primary").addEventListener("click", event => {
+    sendRequest("GET", REQUEST_URL)
+    .then((response) => response.json())
+    .then((json) => {
+      if (true) {
+        console.log(json);
+        window.location.href = "question.html";
+      } else {
+        document.querySelector(".js_btn").click();
+      }
+    });
+  });
 }
-sendRequest("GET", REQUEST_URL)
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+
 
 ////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
+if (document.querySelector(".question")) { 
+  const refs = {
+    radio1: document.getElementById("radio-1"),
+    radio2: document.getElementById("radio-2"),
+    radio3: document.getElementById("radio-3"),
+    radio4: document.getElementById("radio-4"),
+    loader: document.querySelector(".loader"),
+    form: document.querySelector(".form__button"),
+  };
 
-const refs = {
-  radio1: document.getElementById("radio-1"),
-  radio2: document.getElementById("radio-2"),
-  radio3: document.getElementById("radio-3"),
-  radio4: document.getElementById("radio-4"),
-  loader: document.querySelector(".loader"),
-  form: document.querySelector(".form__wrapper"),
-};
+  refs.radio1.addEventListener("change", event => {
+    const collapseElementList = document.querySelectorAll('.collapse')
+    const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+  });
+  refs.radio2.addEventListener("change", event => {
+    if (document.querySelectorAll('.collapse .show')) {
+      const collapseElementList = document.querySelectorAll('.collapse')
+      const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+    }
+    if (refs.radio3.checked || refs.radio4.checked) {
+      refs.radio3.checked = false
+      refs.radio4.checked = false
+    }
+  });
 
-refs.radio1.addEventListener("change", onToggleCheck);
-refs.radio2.addEventListener("change", onToggleCheck);
-refs.radio3.addEventListener("change", onToggleCheck);
-refs.radio4.addEventListener("change", onToggleCheck);
+  /*
+  *   SUBMIT FORM
+  */
+  refs.form.addEventListener("click", event => {
+    
+  });
 
-// // ====================================================================
-// DISABLED
-// =====================================================================
-function onToggleCheck() {
-  if (refs.radio1.checked) {
-    refs.radio3.disabled = false;
-    refs.radio4.disabled = false;
-  } else if (refs.radio2.checked) {
-    refs.radio3.disabled = true;
-    refs.radio4.disabled = true;
+  function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    return (document.location.href = "loader.html");
   }
 }
 
-// ============================================
-// SUBMIT FORM
-////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-refs.form.addEventListener("submit", onFormSubmit);
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-
-  return (document.location.href = "loader.html");
-}
-//////////////////////////////////////////////////////////////////////////////////////////////////
-// ЗАГРУЗКА И СТРАНИЦЫ
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-//
+/*
+*   ЗАГРУЗКА И СТРАНИЦЫ
+*/
 
 // function onLoaderPage() {
 //   setTimeout(function () {
