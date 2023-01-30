847d2a563f90409bd5ed0da258b23b18a5be4068
Fenickss <denis.suxov.1991@mail.ru>
2023-01-30 16:24:15 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,30 @@
 // API=======================================================
 // ===============================================================
 if (document.querySelector(".main")) {
-  
   const REQUEST_URL = "https://jsonplaceholder.typicode.com/users/12";
 
   function sendRequest() {
     return fetch(REQUEST_URL);
   }
 
-  document.querySelector(".tn-btn__primary").addEventListener("click", event => {
-    sendRequest("GET", REQUEST_URL)
-    .then((response) => response.json())
-    .then((json) => {
-      if (true) {
-        console.log(json);
-        window.location.href = "question.html";
-      } else {
-        document.querySelector(".js_btn").click();
-      }
+  document
+    .querySelector(".tn-btn__primary")
+    .addEventListener("click", (event) => {
+      sendRequest("GET", REQUEST_URL)
+        .then((response) => response.json())
+        .then((json) => {
+          if (true) {
+            console.log(json);
+            window.location.href = "question.html";
+          } else {
+            document.querySelector(".js_btn").click();
+          }
+        });
     });
-  });
 }
 
-
 ////////////////////////////////////////////////////////////////////////
-if (document.querySelector(".question")) { 
+if (document.querySelector(".question")) {
   const refs = {
     radio1: document.getElementById("radio-1"),
     radio2: document.getElementById("radio-2"),
@@ -34,27 +34,29 @@ if (document.querySelector(".question")) {
     form: document.querySelector(".form__button"),
   };
 
-  refs.radio1.addEventListener("change", event => {
-    const collapseElementList = document.querySelectorAll('.collapse')
-    const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+  refs.radio1.addEventListener("change", (event) => {
+    const collapseElementList = document.querySelectorAll(".collapse");
+    const collapseList = [...collapseElementList].map(
+      (collapseEl) => new bootstrap.Collapse(collapseEl)
+    );
   });
-  refs.radio2.addEventListener("change", event => {
-    if (document.querySelectorAll('.collapse .show')) {
-      const collapseElementList = document.querySelectorAll('.collapse')
-      const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+  refs.radio2.addEventListener("change", (event) => {
+    if (document.querySelectorAll(".collapse .show")) {
+      const collapseElementList = document.querySelectorAll(".collapse");
+      const collapseList = [...collapseElementList].map(
+        (collapseEl) => new bootstrap.Collapse(collapseEl)
+      );
     }
     if (refs.radio3.checked || refs.radio4.checked) {
-      refs.radio3.checked = false
-      refs.radio4.checked = false
+      refs.radio3.checked = false;
+      refs.radio4.checked = false;
     }
   });
 
   /*
-  *   SUBMIT FORM
-  */
-  refs.form.addEventListener("click", event => {
-    
-  });
+   *   SUBMIT FORM
+   */
+  refs.form.addEventListener("click", (event) => {});
 
   function onFormSubmit(evt) {
     evt.preventDefault();
@@ -63,16 +65,9 @@ if (document.querySelector(".question")) {
   }
 }
 
-
 /*
-*   ЗАГРУЗКА И СТРАНИЦЫ
-*/
-
-// function onLoaderPage() {
-//   setTimeout(function () {
-//     document.location.href = "answer.html";
-//   }, 3000);
-// }
+ *   ЗАГРУЗКА И СТРАНИЦЫ
+ */
 
 // if (document.readyState === "complete") {
 //   // ещё загружается, ждём события
@@ -87,3 +82,10 @@ if (document.querySelector(".question")) {
 // if ((document.location.href = "loader.html")) {
 //   alert();
 // }
+if ((window.location.href = "http://loader.html")) {
+  function onLoaderPage() {
+    setTimeout(function () {
+      window.location.href = "http://answer.html";
+    }, 3000);
+  }
+}
